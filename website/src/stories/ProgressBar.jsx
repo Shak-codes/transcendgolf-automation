7843b0d51badc0cd510ab303047efba4a6b90e02
Shak12345 <shakirrahman81@gmail.com>
2022-08-12 10:52:02 -0400

    Naming cleanup

--- website/src/stories/ProgressBar.jsx
+++ website/src/stories/ProgressBar.jsx
@@ -5,58 +5,64 @@ import './progressBar.css';
 /**
  * Primary UI component for user interaction
  */
-export const ProgressBar = ({ width, height, color, steps, fontSize, stepColor, ...props }) => {
+export const ProgressBar = ({ containerWidth, containerHeight, barColor, nodes, nodeSize, futNodeColor, curNodeColor, prevNodeColor, ...props }) => {
 
-    const [currentStep, setCurrentStep] = useState(1)
+    const [curNode, setcurNode] = useState(1);
     const [animate, setAnimate] = useState(false);
-    const stepValue = 80/steps
+    const stepValue = 80/nodes;
 
-    const updateStep = () => {
+    const updateNode = () => {
         setAnimate(true)
         setTimeout(setAnimate(false), 1000)
-        const currentStepCopy = currentStep;
-        if (currentStepCopy < steps + 1) {
-            setCurrentStep(currentStepCopy + 1)
-        } else setCurrentStep(1)
+        const curNodeCopy = curNode;
+        if (curNodeCopy < nodes + 1) {
+            setcurNode(curNodeCopy + 1)
+        } else setcurNode(1)
     }
 
-    const checkpoints = useMemo(() => {
-        return [...Array(steps+1)].map((element, index) => (
-            <circle className={index + 1 === currentStep ? "currentStep" : "step"} cx={50} cy={`${10+(stepValue*index)}%`} r={`${width}px`} fill={stepColor} />
+    const node = useMemo(() => {
+        return [...Array(nodes+1)].map((element, index) => (
+            <circle className={index + 1 === curNode ? "curNode" : index + 1 < curNode ? "prevNode" : "futNode"} cx={50} cy={`${10+(stepValue*index)}%`} r={`${containerWidth}px`} 
+            fill={index + 1 === curNode ? `${curNodeColor}` : index + 1 < curNode ? `${prevNodeColor}` : `${futNodeColor}`}/>
             )
         )
-    }, [currentStep, stepColor, stepValue, steps, width])
+    }, [nodes, curNode, stepValue, containerWidth, curNodeColor, prevNodeColor, futNodeColor])
 
-    const stepAnimation = useMemo(() => {
-        return <circle className={!animate ? "stepNoAnim" : "stepAnimation"} cx={50} cy={`${10+(stepValue*(currentStep-1))}%`} r={`${width/1.5}px`} fill={'black'} />
+    const nodeAnimation = useMemo(() => {
+        return <circle className={!animate ? "staticNode" : "movingNode"} cx={50} cy={`${10+(stepValue*(curNode-1))}%`} r={`${containerWidth/1.5}px`} fill={curNodeColor} />
       
-    }, [animate, currentStep, stepValue, width])
+    }, [animate, stepValue, curNode, containerWidth, curNodeColor])
 
     return (
-        <div id="test" onClick={updateStep} style={{height: height, width: 100}}>
+        <div id="test" onClick={updateNode} style={{width: 100, height: containerHeight}}>
         <svg id="chart" width="100%" height="100%">
-            <line style={{strokeWidth: 2, stroke: color, }} className="bar" x1={50} y1="10%" x2={50} y2="90%" strokeLinecap="round"></line>
-            {stepAnimation}
-            {checkpoints}
+            <line style={{strokeWidth: 2, stroke: barColor }} className="bar" x1={50} y1="10%" x2={50} y2="90%" strokeLinecap="round"></line>
+            {nodeAnimation}
+            {node}
         </svg>
         </div>
     );
 };
 
 ProgressBar.propTypes = {
-    width: PropTypes.number,
-    height: PropTypes.number,
-    color: PropTypes.string,
-    steps: PropTypes.number,
-    fontSize: PropTypes.number,
-    stepColor: PropTypes.string
+    containerWidth: PropTypes.number,
+    containerHeight: PropTypes.number,
+    barColor: PropTypes.string,
+    nodes: PropTypes.number,
+    nodeSize: PropTypes.number,
+    futNodeColor: PropTypes.string,
+    curNodeColor: PropTypes.string,
+    prevNodeColor: PropTypes.string,
 };
 
 ProgressBar.defaultProps = {
-    width: 5,
-    height: 100,
-    color: 'black',
-    steps: 5,
-    fontSize: 10,
-    stepColor: 'aquamarine'
+    containerWidth: 5,
+    containerHeight: 100,
+    barColor: 'aquamarine',
+    nodes: 5,
+    nodeSize: 20,
+    futNodeColor: 'black',
+    curNodeColor: 'aquamarine',
+    prevNodeColor: 'aquamarine',
+    
 };
