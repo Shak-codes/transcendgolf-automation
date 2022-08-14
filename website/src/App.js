4baa3987179b687b025c422478f8b870a065224f
Shak12345 <shakirrahman81@gmail.com>
2022-08-14 19:53:57 -0400

    Updated website

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 import { ProgressBar } from './stories/ProgressBar';
-import { Card } from './stories/Card';
 
 import useWindowDimensions from './stories/assets/dimensions';
 
@@ -9,38 +8,26 @@ function App() {
 
   const { height, width } = useWindowDimensions();
   const [step, setStep] = useState(1)
+  const text = 'Skedda removes the hassle of manually managing \nthe booking and scheduling of your spaces.\nThe perfect booking platform for your rooms, \ndesks, studios, labs, courts and more.'
 
   return (
     <div className='background'>
-      <div className='cardContainer'>
-        {step === 1 && 
-        <>
-        <Card width={width/1.5} height={height/1.5} backgroundColor="#ffffff" shadowColor='black' label="Skedda" labelSize={108}/>
-        <div className='skeddabg'></div>
-        <p>• User makes reservation using our Skedda application</p>
-        <p>• Skedda saves the data</p>
-        <p>- Month</p>
-        <p>- Day</p>
-        <p>- Time</p>
-        </>
-        }
-        {step === 2 && 
-        <Card width={width/1.5} height={height/1.5} backgroundColor="#ffffff" shadowColor='black' label="Skedda" labelSize={108}/>
-        }
-        {step === 3 && 
-        <Card width={width/1.5} height={height/1.5} backgroundColor="#ffffff" shadowColor='black' label="Skedda" labelSize={108}/>
-        }
-        {step === 4 && 
-        <Card width={width/1.5} height={height/1.5} backgroundColor="#ffffff" shadowColor='black' label="Skedda" labelSize={108}/>
-        }
-        {step === 5 && 
-        <Card width={width/1.5} height={height/1.5} backgroundColor="#ffffff" shadowColor='black' label="Skedda" labelSize={108}/>
-        }
-        {step === 6 && 
-        <Card width={width/1.5} height={height/1.5} backgroundColor="#ffffff" shadowColor='black' label="Skedda" labelSize={108}/>
-        }
+      <div className='skeddabg'></div>
+      <div className='textContainer'>
+        <h1 className='title'>BOOKINGS SIMPLIFIED</h1>
+        <p className='text'>{text}</p>
       </div>
-      <ProgressBar width='40' height="100%" color="#cbf3f0" steps={8} fontSize={16} stepColor="#2ec4b6"/>
+      <ProgressBar
+        className='bar'
+        curNode={step}
+        containerHeight="25%"
+        nodes={3}
+        nodeSize={12}
+        barColor="#d7e3df"
+        barWidth={0.5}
+        curNodeColor="#ffffff"
+        nodeColor="#ffffff"
+      />
     </div>
   );
 }
